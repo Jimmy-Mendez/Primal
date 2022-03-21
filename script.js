9dda73f41b3af3da8fe22e85552554baa2840c07
Jimmy Mendez <54858901+Jimmy-Mendez@users.noreply.github.com>
2022-03-20 23:04:50 -0400

    update

--- script.js
+++ script.js
@@ -53,17 +53,18 @@ document.addEventListener("keyup", (e) => {
 })
 
 function insertNumber (pressedKey) {
-    if (nextNumber === 5) {
+    if (nextLetter === 5) {
         return
     }
     pressedKey = pressedKey.toLowerCase()
 
-    let row = document.getElementsByClassName("number-row")[6 - guessesRemaining]
-    let box = row.children[nextNumber]
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    let box = row.children[nextLetter]
+    animateCSS(box, "pulse")
     box.textContent = pressedKey
     box.classList.add("filled-box")
     currentGuess.push(pressedKey)
-    nextNumber += 1
+    nextLetter += 1
 }
 
 function deleteNumber () {
@@ -110,7 +111,7 @@ function checkGuess () {
     }
 
     if (!primes.includes(parseInt(guessString))) {
-        alert("Not a prime!")
+        alert("Not a 5 digit prime!")
         return
     }
 
@@ -141,6 +142,8 @@ function checkGuess () {
 
         let delay = 250 * i
         setTimeout(()=> {
+            //flip box
+            animateCSS(box, 'flipInX')
             //shade box
             box.style.backgroundColor = numberColor
             shadeKeyBoard(number, numberColor)
@@ -178,3 +181,23 @@ document.getElementById("keyboard-cont").addEventListener("click", (e) => {
 
     document.dispatchEvent(new KeyboardEvent("keyup", {'key': key}))
 })
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    // const node = document.querySelector(element);
+    const node = element
+    node.style.setProperty('--animate-duration', '0.3s');
+    
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+});
